cfb4eb87cbc2b8c3645e6333c768fb83557e2a1b
Trần Minh Sang <sangtranminh@thegioididong.com>
2024-06-06 17:53:01 +0700

    edit themes

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,18 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  --{ import = "astrocommunity.pack.python" },
+  --{ import = "astrocommunity.pack.json" },
   --{ import = "astrocommunity.pack.cmake" },
+  --  { import = "astrocommunity.colorscheme.aurora" },
+  --  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  --  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.cyberdream-nvim" },
+  --  { import = "astrocommunity.colorscheme.bluloco-nvim" },
+  --  { import = "astrocommunity.colorscheme.citruszest-nvim" },
+  --  { import = "astrocommunity.colorscheme.miasma-nvim" },
+  { import = "astrocommunity.colorscheme.neofusion-nvim" },
+  --  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
   -- import/override with your plugins folder
 }
